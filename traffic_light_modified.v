b884ebe56cae457a0f0316f4e4ea2ee4d1bdf92f
vinitjangir7297 <vinitjangir7297@gmail.com>
2025-12-07 19:55:19 +0530

    Add files via upload

--- traffic_light_modified.v
+++ traffic_light_modified.v
@@ -10,12 +10,12 @@ reg [1:0]state; // present state register
 reg [1:0]next_state; //next state register
 reg [2:0]count; //register for my count
 always@(posedge clk) begin
-if(rst) begin 
+if(rst) begin //on rst red light will glow and count becomes zero
 count<=0;
 state<=S0;
 end
 else begin
-if((state==S0 && count==4) || (state==S1 && count==4) || (state==S2 && count==1)) begin  // condition for count to become 0 and
+if((state==S0 && count==4) || (state==S1 && count==4) || (state==S2 && count==1)) begin  // condition for count to become 0 and when state will change
 count<=0;
 state<=next_state;
 end
@@ -23,30 +23,30 @@ else
 count<=count+1;
 end
 end
-always@(*) begin
+always@(*) begin // always block for my combinational part that which will be my next state on count condition
 next_state=state;
 case(state)
 S0: begin
-if(count==4) 
+if(count==4) // as given for red light after 5 cycle it gets off if rst is not there
 next_state=S1;
 else
 next_state=S0;
 end
 S1:begin
-if(count==4) 
+if(count==4) // as given for green light after 5 cycle it gets off
 next_state=S2;
 else
 next_state=S1;
 end
 S2:begin
-if(count==1)
+if(count==1) // as given for yellow light after 2 cycle it gets off 
 next_state=S0;
 else
 next_state=S2;
 end
 endcase
 end
-always@(*)begin
+always@(*)begin // always block for my ouput part when & which light will be ON
 red=0;
 yellow=0;
 green=0;
